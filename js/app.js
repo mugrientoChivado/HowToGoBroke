121b97d7a91eec0ab9977129935612d40e004b92
Mugriento Chivado <mugriento.chivado@proton.me>
2024-03-11 20:17:12 -0300

    Added some more blurb in the home page (I think it's finished, or at least almost finished). Also added crypto page. :)

--- js/app.js
+++ js/app.js
@@ -12,9 +12,11 @@ let midiPlay = undefined;
 let paused = false;
 let stopped = false;
 let playingAudio = false;
+let navButtons = undefined;
+let homeDancingCatInterval = undefined;
 
-window.addEventListener("load", () => {
-    let navButtons = document.querySelectorAll(".nav-link");
+document.addEventListener("DOMContentLoaded", () => {
+    navButtons = document.querySelectorAll(".nav-link");
     for(let i = 0; i < navButtons.length; i++) {
         navButtons[i].addEventListener("click", navigateWithinContainer);
     }
@@ -39,23 +41,20 @@ async function loadPage(page) {
     pageName = pageName[pageName.length-1];
 
     switch(pageName) {
-        case "home.html": {
-            midiPlay = "Joost-Cant-Get-Enoof.mid";
-            setInterval(() => {
-                document.querySelector("#dancing-cat-slide-1").src = "./img/"+dancingCatImages1[dancingCatImages1iterator];
-                if(dancingCatImages1iterator < dancingCatImages1.length - 1)
-                    dancingCatImages1iterator++;
-                else
-                    dancingCatImages1iterator = 0;
-            }, 2000);
+        case "home.html":
+            home();
             break;
-        }
         case "art-gallery.html":
             playingAudio = true;
+            break;
+        case "crypto.html":
+            crypto();
             break;
         default:
             break;
     }
+    if(pageName !== "home.html")
+        clearInterval(homeDancingCatInterval);
 
     if(midiPlay != undefined)
         MIDIjs.play("./mid/"+midiPlay);
@@ -107,4 +106,30 @@ function stopMusicPlayback() {
     stopAudioMusics();
     stopped = true;
     paused = true;
+}
+
+function home() {
+    midiPlay = "Joost-Cant-Get-Enoof.mid";
+    homeDancingCatInterval = setInterval(() => {
+        document.querySelector("#dancing-cat-slide-1").src = "./img/"+dancingCatImages1[dancingCatImages1iterator];
+        if(dancingCatImages1iterator < dancingCatImages1.length - 1)
+            dancingCatImages1iterator++;
+        else
+            dancingCatImages1iterator = 0;
+    }, 2000);
+    document.querySelector("#artists-link-art").addEventListener("click", (event) => {
+        event.preventDefault();
+        navButtons[1].click();
+    });
+    document.querySelector("#artists-link-music").addEventListener("click", (event) => {
+        event.preventDefault();
+        navButtons[2].click();
+    });
+}
+
+function crypto() {
+    let videos = document.getElementsByTagName("video");
+    for(let i = 0; i < videos.length; i++) {
+        videos[i].play();
+    }
 }
